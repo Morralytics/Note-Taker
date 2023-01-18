b0a71db3303b89a12204865b3784dc08bc3656bb
Nick Morris <nicholasmorris2599@gmail.com>
2023-01-17 21:30:14 -0800

    Allowing the app to start and listen on port 3001.

--- Develop/public/server.js
+++ Develop/public/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 
 const PORT = process.env.port || 3001;
+
 const app = express();
 
 app.use(express.static('public'));
@@ -13,3 +14,7 @@ app.get('/', (req, res) => {
 app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/notes.html'));
 });
+
+app.listen(PORT, () => 
+    console.log(`App listening at http://localhost:${PORT}`)
+);
