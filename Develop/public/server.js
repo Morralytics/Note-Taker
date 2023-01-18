7373e57f76a3dc092baab753022496a7601e6133
Nick Morris <nicholasmorris2599@gmail.com>
2023-01-17 21:27:30 -0800

    Added get responses for both the home page and note path.

--- Develop/public/server.js
+++ Develop/public/server.js
@@ -3,3 +3,12 @@ const path = require('path');
 
 const app = express();
 
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/index.html'));
+});
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/notes.html'));
+});
