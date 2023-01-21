d10aa6e87ade99ce0cf9c6a34abb83189495168c
Nick Morris <nicholasmorris2599@gmail.com>
2023-01-21 00:45:27 -0800

    Added a read and append function for data injection.

--- Develop/helpers/fsUtils.js
+++ Develop/helpers/fsUtils.js
@@ -1,3 +1,4 @@
+const e = require('express');
 const fs = require('fs');
 const util = require('util');
 
@@ -5,7 +6,21 @@ const util = require('util');
 const readFile = util.promisify(fs.readFile);
 
 // Function that writes our file given the where and what
-const writeToFile = (path, content) =>
+const writeToFile = (path, content) => {
     fs.writeFile(path, JSON.stringify(content), (err) => {
         err ? console.log(err) : console.log(`Content written to ${path}`);
     });
+};
+
+// Function that reads the file and can append the same data to a file
+const readAndAppend = (file, content) => {
+    fs.readFile(file, 'utf8', (err, data) => {
+        if (err) {
+            console.error(err);
+        } else {
+            const parsedData = JSON.parse(data);
+            parsedData.push(content);
+            writeToFile(file, parsedData)
+        }
+    });
+};
