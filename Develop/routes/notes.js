06595823ed28af36a13694cd98694b43fdf93a28
Nick Morris <nicholasmorris2599@gmail.com>
2023-01-21 15:36:08 -0800

    Notes route displays the saved test text in the json file.

--- Develop/routes/notes.js
+++ Develop/routes/notes.js
@@ -3,6 +3,7 @@ const notes = require('express').Router();
 const { readFromFile, readAndAppend } = require('../helpers/fsUtils');
 const uuid = require('../helpers/uuid');
 
+// Properly reads data from the json file and has the response of the promise parse it for viewing
 notes.get('/', (req, res) => {
     console.log(`${req.method} request received for notes.`);
     readFromFile('./db/db.json')
